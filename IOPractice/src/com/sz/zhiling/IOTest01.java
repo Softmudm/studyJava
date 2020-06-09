f58ceeaebf1399f2b6b0ecf04219eaa65b062020
softmudum <1136470472@qq.com>
2020-06-09 14:45:30 +0800

    IO缓存流和对象流练习

--- IOPractice/src/com/sz/zhiling/IOTest01.java
+++ IOPractice/src/com/sz/zhiling/IOTest01.java
@@ -49,5 +49,105 @@ public class IOTest01 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        /**
+         * 缓冲流
+         */
+        FileInputStream fis1 = null;
+        FileOutputStream fos1 = null;
+        BufferedInputStream bis1 = null;
+        BufferedOutputStream bos1 = null;
+        try {
+            fis1 = new FileInputStream("9.txt");
+            fos1 = new FileOutputStream("10.txt");
+            bis1 = new BufferedInputStream(fis1);
+            bos1 = new BufferedOutputStream(fos1);
+            int num = bis1.read();
+            while(num != -1){
+                bos1.write(num);
+                num = bis1.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bos1.flush();
+                bos1.close();
+                bis1.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Writer w = null;
+        Reader r = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            r = new FileReader("11.txt");
+            w = new FileWriter("12.txt",true);//这里的true是不覆盖而是在12.txt内追加.
+            br = new BufferedReader(r);
+            bw = new BufferedWriter(w);
+            String str = br.readLine();
+            while (str != null){
+                bw.write(str);
+                str = br.readLine();
+            }
+            System.out.println("使用缓冲字符流复制文件成功！");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bw.flush();
+                bw.close();
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        /**
+         * 对象流
+         */
+        OutputStream outputStream = null;
+        InputStream inputStream = null;
+        ObjectInputStream objectInputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+        try {
+            outputStream = new FileOutputStream("13.txt");//序列化到这个文件
+            inputStream = new FileInputStream("13.txt");//从这个文件反序列化
+            objectOutputStream = new ObjectOutputStream(outputStream);
+            objectInputStream = new ObjectInputStream(inputStream);
+            User user = new User("张三",88);
+            objectOutputStream.writeObject(user);
+           /* byte[] bt = new byte[1024*8];
+            int num = inputStream.read(bt);
+            while(num != -1){
+                for (byte b : bt) {
+                    System.out.println((char)b);
+                }
+                num = inputStream.read(bt);
+            }*/
+            Object o = objectInputStream.readObject();
+            System.out.println((User)o);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                objectOutputStream.flush();
+                objectOutputStream.close();
+                objectInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
--- IOPractice/src/com/sz/zhiling/IOTest02.java
+++ IOPractice/src/com/sz/zhiling/IOTest02.java
@@ -3,7 +3,7 @@ package com.sz.zhiling;
 import java.io.*;
 
 public class IOTest02 {
-    public static void copyFile(String f1,String f2){
+    public static void copyFile(String f1,String f2){//将f1路径的文件复制到f2路径
         InputStream inputStream = null;
         OutputStream outputStream = null;
         try {
@@ -28,20 +28,38 @@ public class IOTest02 {
             }
         }
     }
-    public static void zuoYe(String s1,String s2){//s1源文件,s2目标文件
+    public static void copy(String s1,String s2){//s1源文件,s2目标文件。可以复制文件夹
         File f1 = new File(s1);
+        String str1 = s2+"/"+f1.getName();
         if(f1.isDirectory()){
-            File f2 = new File(s2+"/"+f1.getName());
+            File f2 = new File(str1);
             f2.mkdir();
             String[] strings = f1.list();
             for (String string : strings) {
-                zuoYe(s1+"/"+string,s2+"/"+f2.getName());
+                copy(s1+"/"+string,str1);
             }
         }else{
-            copyFile(s1,s2+"/"+f1.getName());
+            copyFile(s1,str1);
+        }
+    }
+    public static void delete(String s){//删除s路径出的文件或文件夹
+        File file1 = new File(s);
+        if(file1.isDirectory()){
+            String[] str = file1.list();
+            if(str != null){
+                for (String s1 : str) {
+                    delete(file1+"/"+s1);
+                }
+            }else{
+                file1.delete();
+            }
+            file1.delete();
+        }else{
+            file1.delete();
         }
     }
     public static void main(String[] args) {
-        zuoYe("D:/222","F:");
+        copy("D:/222","F:");
+        delete("F:/222");
     }
 }
