1d0e8bae5781def4d6f8ccf5f5e816aad54db7dd
softmudum <1136470472@qq.com>
2020-09-04 15:47:50 +0800

    返回前端的数据由string改为json

--- OnlineExam01/src/main/java/com/sz/wx/controller/ItemsController.java
+++ OnlineExam01/src/main/java/com/sz/wx/controller/ItemsController.java
@@ -2,6 +2,7 @@ package com.sz.wx.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
 import com.sz.wx.model.Items;
 import com.sz.wx.service.ItemsService;
@@ -31,7 +32,7 @@ public class ItemsController {
     @ApiImplicitParams({@ApiImplicitParam(name = "paperid", value = "要查询的试卷id",required = true,dataType = "int")})
     //查询试卷具体题目
     @RequestMapping("/getItemsListByPaperid")
-    public String getItemsListByPaperid(Integer paperid){
+    public JSONArray getItemsListByPaperid(Integer paperid){
         //实例化过滤器对象在return是用来指定过滤字段
         SimplePropertyPreFilter filter = new SimplePropertyPreFilter();
         //这里在序列化时排除答案和解析不传到页面上
@@ -41,6 +42,6 @@ public class ItemsController {
         List<Items> list = itemsService.getItemsListByPaperid(paperid);
         //打乱题目顺序
         Collections.shuffle(list);
-        return JSON.toJSONString(list,filter);
+        return JSON.parseArray(JSON.toJSONString(list,filter));
     }
 }
